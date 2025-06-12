4b2edfe78c189174a1b8eef513773f54da67f747
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2025-06-11 20:51:53 -0400

    Building multiple pieces working.

--- Source/AutoSupport/Private/Buildables/BuildableAutoSupport.cpp
+++ Source/AutoSupport/Private/Buildables/BuildableAutoSupport.cpp
@@ -4,6 +4,7 @@
 
 #include "FGBuildingDescriptor.h"
 #include "FGColoredInstanceMeshProxy.h"
+#include "ModLogging.h"
 
 ABuildableAutoSupport::ABuildableAutoSupport()
 {
@@ -11,29 +12,155 @@ ABuildableAutoSupport::ABuildableAutoSupport()
 	InstancedMeshProxy->SetupAttachment(RootComponent);
 }
 
-void ABuildableAutoSupport::BuildSupports()
+bool ABuildableAutoSupport::IsBuildable(const FVector& PartCounts) const
 {
-	if (!IsValid(MiddlePartDescriptor))
+	if (PartCounts.IsNearlyZero())
 	{
-		// we're building either the top or bottom
-		return;
+		return false;
 	}
-
-	auto* Buildables = AFGBuildableSubsystem::Get(GetWorld());
-
-	BuildPart(Buildables, MiddlePartDescriptor, GetActorTransform());
-}
-
-void ABuildableAutoSupport::TraceDown()
-{
 	
+	// TODO: check player has enough resources to build
+	return true;
 }
 
-void ABuildableAutoSupport::BuildPart(AFGBuildableSubsystem* Buildables, const TSubclassOf<UFGBuildingDescriptor> PartDescriptor, const FTransform& Transform)
+void ABuildableAutoSupport::BuildParts(
+	AFGBuildableSubsystem* Buildables,
+	TSubclassOf<UFGBuildingDescriptor> PartDescriptor,
+	const int32 Count,
+	const FVector& Size,
+	FTransform& WorkingTransform)
 {
 	const TSubclassOf<AFGBuildable> BuildableClass = UFGBuildingDescriptor::GetBuildableClass(PartDescriptor);
 	
-	auto* Buildable = Buildables->BeginSpawnBuildable(BuildableClass, Transform);
+	for (auto i = 0; i < Count; ++i)
+	{
+		MOD_LOG(Verbose, TEXT("BuildableAutoSupport::BuildParts Spawning part. Start Transform: %s"), *WorkingTransform.ToString());
+		
+		// Spawn the part
+		auto* Buildable = Buildables->BeginSpawnBuildable(BuildableClass, WorkingTransform);
+		Buildable->FinishSpawning(WorkingTransform);
 
-	Buildable->FinishSpawning(Transform);
+		// Update the transform
+		WorkingTransform.AddToTranslation(FVector(0, 0, -1 * Size.Z));
+		MOD_LOG(Verbose, TEXT("BuildableAutoSupport::BuildParts End Transform: %s"), *WorkingTransform.ToString());
+	}
 }
+
+void ABuildableAutoSupport::BuildSupports()
+{
+	if (!MiddlePartDescriptor && !StartPartDescriptor && !EndPartDescriptor)
+	{
+		// Nothing to build.
+		return;
+	}
+	
+	// Trace to know how much we're going to build.
+	auto BuildDistance = FMath::Min(Trace(), MaxBuildDistance);
+
+	MOD_LOG(Verbose, TEXT("BuildableAutoSupport::BuildSupports BuildDistance: %f"), BuildDistance);
+
+	if (FMath::IsNearlyZero(BuildDistance))
+	{
+		// No room to build.
+		return;
+	}
+
+	FVector PartCounts;
+	FBox TopBox, MidBox, BottomBox;
+
+	PlanBuild(BuildDistance, PartCounts, TopBox, MidBox, BottomBox);
+	
+	// Check that the player building it has enough resources
+	if (!IsBuildable(PartCounts))
+	{
+		return;
+	}
+
+	// Build the parts
+	auto* Buildables = AFGBuildableSubsystem::Get(GetWorld());
+	auto WorkingTransform = GetActorTransform();
+
+	if (PartCounts.X > 0)
+	{
+		BuildParts(Buildables, StartPartDescriptor, PartCounts.X, TopBox.GetSize(), WorkingTransform);
+	}
+
+	if (PartCounts.Y > 0)
+	{
+		BuildParts(Buildables, MiddlePartDescriptor, PartCounts.Y, MidBox.GetSize(), WorkingTransform);
+	}
+
+	if (PartCounts.Z > 0)
+	{
+		BuildParts(Buildables, EndPartDescriptor, PartCounts.Z, BottomBox.GetSize(), WorkingTransform);
+	}
+}
+
+float ABuildableAutoSupport::Trace()
+{
+	return 2000;
+}
+
+void ABuildableAutoSupport::PlanBuild(float BuildDistance, OUT FVector& OutPartCounts, OUT FBox& OutStartBox, OUT FBox& OutMidBox, OUT FBox& OutEndBox) const
+{
+	OutPartCounts = FVector::ZeroVector;
+
+	if (!MiddlePartDescriptor && !StartPartDescriptor && !EndPartDescriptor)
+	{
+		return;
+	}
+
+	// Start with the top because that's where the auto support is planted.
+	if (StartPartDescriptor)
+	{
+		GetBuildableSize(StartPartDescriptor, OutStartBox);
+		const auto Size = OutStartBox.GetSize();
+
+		MOD_LOG(Verbose, TEXT("BuildableAutoSupport::PlanBuild Start Size: %s"), *Size.ToString());
+		BuildDistance -= Size.Z;
+
+		if (BuildDistance < 0)
+		{
+			// Not enough room (TODO: might be able to in a small negative range)
+			return;
+		}
+		
+		OutPartCounts.X = 1;
+	}
+
+	// Do the end next. There may not be enough room for mid pieces.
+	if (EndPartDescriptor)
+	{
+		GetBuildableSize(EndPartDescriptor, OutEndBox);
+		const auto Size = OutEndBox.GetSize();
+
+		MOD_LOG(Verbose, TEXT("BuildableAutoSupport::PlanBuild End Size: %s"), *Size.ToString());
+		BuildDistance -= Size.Z;
+
+		if (BuildDistance < 0)
+		{
+			// Not enough room (TODO: might be able to in a small negative range)
+			return;
+		}
+
+		OutPartCounts.Z = 1;
+	}
+	
+	if (MiddlePartDescriptor)
+	{
+		GetBuildableSize(MiddlePartDescriptor, OutMidBox);
+		const auto Size = OutMidBox.GetSize();
+		
+		auto NumMiddleParts = static_cast<int32>(BuildDistance / Size.Z);
+		MOD_LOG(Verbose, TEXT("BuildableAutoSupport::PlanBuild Mid Size: %s, Num Mid: %d"), *Size.ToString(), NumMiddleParts);
+		
+		OutPartCounts.Y = NumMiddleParts;
+	}
+}
+
+void ABuildableAutoSupport::GetBuildableSize(const TSubclassOf<UFGBuildingDescriptor> PartDescriptor, OUT FBox& OutBox)
+{
+	const auto Buildable = GetDefault<AFGBuildable>(UFGBuildingDescriptor::GetBuildableClass(PartDescriptor));
+	OutBox = Buildable->GetCombinedClearanceBox();
+}
+
--- Source/AutoSupport/Public/Buildables/BuildableAutoSupport.h
+++ Source/AutoSupport/Public/Buildables/BuildableAutoSupport.h
@@ -16,23 +16,57 @@ class AUTOSUPPORT_API ABuildableAutoSupport : public AFGBuildable
 public:
 	ABuildableAutoSupport();
 
+	/**
+	 * The
+	 */
 	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Auto Supports")
-	TSubclassOf<UFGBuildingDescriptor> TopPartDescriptor;
+	TSubclassOf<UFGBuildingDescriptor> StartPartDescriptor;
 
 	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Auto Supports")
 	TSubclassOf<UFGBuildingDescriptor> MiddlePartDescriptor;
 
 	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Auto Supports")
-	TSubclassOf<UFGBuildingDescriptor> BottomPartDescriptor;
+	TSubclassOf<UFGBuildingDescriptor> EndPartDescriptor;
 
+	/**
+	 * The maximum distance the auto support can build.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Auto Supports")
+	float MaxBuildDistance = 10000.0f;
+
+	/**
+	 * Builds the supports based on the provided configuration.
+	 */
 	UFUNCTION(BlueprintCallable)
 	void BuildSupports();
 	
 protected:
+	/**
+	 * The buildable mesh proxy.
+	 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Buildable Components")
 	TObjectPtr<UFGColoredInstanceMeshProxy> InstancedMeshProxy;
+	
+	/**
+	 * Traces to the terrian.
+	 * @return The free distance.
+	 */
+	float Trace();
 
-	void TraceDown();
+	/**
+	 * 
+	 * @param BuildDistance The distance to build.
+	 * @param OutPartCounts The output part counts. X is for the start part, Y is for the middle part, Z is for the end part.
+	 * @param OutStartBox
+	 * @param OutMidBox
+	 * @param OutEndBox
+	 */
+	void PlanBuild(float BuildDistance, OUT FVector& OutPartCounts, FBox& OutStartBox, FBox& OutMidBox, FBox& OutEndBox) const;
 
-	void BuildPart(AFGBuildableSubsystem* Buildables, TSubclassOf<UFGBuildingDescriptor> PartDescriptor, const FTransform& Transform);
+	bool IsBuildable(const FVector& PartCounts) const;
+
+	void BuildParts(AFGBuildableSubsystem* Buildables, TSubclassOf<UFGBuildingDescriptor> PartDescriptor, int32 Count, const FVector& Size, FTransform&
+		WorkingTransform);
+
+	static void GetBuildableSize(TSubclassOf<UFGBuildingDescriptor> PartDescriptor, OUT FBox& OutBox);
 };
